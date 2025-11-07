35489a38f1a14a0d14a07d2977f4acf5fd8678ae
sebotero18 <ruedasebas18@gmail.com>
2025-11-07 17:48:03 -0500

    Add files via upload

---
+++ #Descuento1500.py
@@ -0,0 +1,13 @@
+#Descuento1500
+
+CostArt=float(input("digite el valor de su articulo"))
+
+CostArtDesc=CostArt-(CostArt*0.05)
+
+if(CostArt>=150000):
+    print("el costo de tu articulo es de ",CostArtDesc)
+else:
+    print("el costo de tu articulo es de ",CostArt)
+
+
+
